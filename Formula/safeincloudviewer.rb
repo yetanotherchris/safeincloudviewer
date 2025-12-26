9e10a81adfc20eff8152d5720a96e1b0e968ead4
yetanotherchris <54912+yetanotherchris@users.noreply.github.com>
2025-12-26 00:41:05 +0000

    Update scoop and homebrew files to version 1.0.2

--- Formula/safeincloudviewer.rb
+++ Formula/safeincloudviewer.rb
@@ -1,33 +1,33 @@
 class Safeincloudviewer < Formula
   desc "A command line tool for viewing SafeInCloud password database files"
   homepage "https://github.com/yetanotherchris/safeincloudviewer"
-  version "1.0.1"
+  version "1.0.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yetanotherchris/safeincloudviewer/releases/download/v1.0.1/safeincloudviewer-v1.0.1-osx-arm64"
-      sha256 "04c096faeaa17b5073eb205875341ebc82a5c9eeeac0e3765c108408b4bf5af2"
+      url "https://github.com/yetanotherchris/safeincloudviewer/releases/download/v1.0.2/safeincloudviewer-v1.0.2-osx-arm64"
+      sha256 "4c70b76d6f4cebe7674bfd2e1a43293ca02cf1384cd2f657f69a20ab7b91870e"
     else
-      url "https://github.com/yetanotherchris/safeincloudviewer/releases/download/v1.0.1/safeincloudviewer-v1.0.1-osx-x64"
-      sha256 "b147888fc19880e49f06736d3a951a9855e680e7b3dfd1c9dd477c22dcd33233"
+      url "https://github.com/yetanotherchris/safeincloudviewer/releases/download/v1.0.2/safeincloudviewer-v1.0.2-osx-x64"
+      sha256 "bbf7360ec4020baaed4fc63ef481f1b59d720a25a1829944ad4c4174c3429e1c"
     end
   end
 
   on_linux do
-    url "https://github.com/yetanotherchris/safeincloudviewer/releases/download/v1.0.1/safeincloudviewer-v1.0.1-linux-x64"
-    sha256 "e56e6917278056875416703c5d7d3c9bcee22b6d4363162e804d4b53e7f9fa4f"
+    url "https://github.com/yetanotherchris/safeincloudviewer/releases/download/v1.0.2/safeincloudviewer-v1.0.2-linux-x64"
+    sha256 "49b9f7176051d4cfe50b1f13f706442f75970e1ad26a57fe8662c0994192a403"
   end
 
   def install
     if OS.mac?
       if Hardware::CPU.arm?
-        bin.install "safeincloudviewer-v1.0.1-osx-arm64" => "safeincloudviewer"
+        bin.install "safeincloudviewer-v1.0.2-osx-arm64" => "safeincloudviewer"
       else
-        bin.install "safeincloudviewer-v1.0.1-osx-x64" => "safeincloudviewer"
+        bin.install "safeincloudviewer-v1.0.2-osx-x64" => "safeincloudviewer"
       end
     else
-      bin.install "safeincloudviewer-v1.0.1-linux-x64" => "safeincloudviewer"
+      bin.install "safeincloudviewer-v1.0.2-linux-x64" => "safeincloudviewer"
     end
   end
 
@@ -35,4 +35,5 @@ class Safeincloudviewer < Formula
     assert_match "SafeInCloud.db", shell_output("#{bin}/safeincloudviewer 2>&1", 1)
   end
 end
+
 
